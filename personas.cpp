f698faf7b0b57d6f5a707ee481de898837b44b71
LuisEnriqueMedellinCordero <50467433+LuisEnriqueMedellinCordero@users.noreply.github.com>
2019-06-07 21:22:30 -0500

    Examen 1° parcial
    este programa es conformado por partes de programas pasados y muestra abre los datos de un archivo con saltos de linea y muestra si es hombre y mujer dependiendo del genero

---
+++ personas.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class persona {
+	string nombre;
+	int edad;
+	char genero;
+	string direccion;
+	string telefono;
+	string email;
+	string muestra;
+	
+public:
+	string LeeTxt(string archivo);
+	void LeeArchivo();
+};
+
+
+string persona::LeeTxt(string archivo) {
+	string texto, linea;
+	ifstream miArchivo(archivo);
+	if (miArchivo.is_open()) {
+		texto = "";
+		texto += linea;
+		while (getline(miArchivo, linea)) {
+			texto += linea;
+		}
+	}
+		miArchivo.close();
+	return texto;
+}
+
+void persona::LeeArchivo() {
+	string token, txt;
+	token=LeeTxt("C:\\Users\\Medellin\\persona.txt");
+	/*cout << token;*/
+	string delimita = ",";
+	size_t pos = 0;
+	while ((pos = token.find(delimita)) != string::npos) {
+		txt = token.substr(0, pos);
+		cout << txt << endl;
+		token.erase(0, pos + delimita.length());
+	}
+	
+	/*cout << LeeTxt("C:\\Users\\Medellin\\persona.txt");*/
+}
+
+
+int main()
+{
+	persona Enrique;
+	Enrique.LeeArchivo();
+}
